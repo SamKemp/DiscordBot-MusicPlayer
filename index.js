9c2c571a4ff45c08d44e0d5f9e1754d4d7f785e1
Sam Kemp <sam@samkemp.me>
2022-02-17 00:13:49 +0000

    Added registar for global commands

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const { bot_token } = require('./config.json');
+const { bot_token, clientId } = require('./config.json');
 
 const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 client.commands = new Collection();
+const commands = [];
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -13,7 +16,26 @@ for (const file of commandFiles) {
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
+  commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '9' }).setToken(bot_token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	}
+  catch (error) {
+		console.error(error);
+	}
+})();
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
